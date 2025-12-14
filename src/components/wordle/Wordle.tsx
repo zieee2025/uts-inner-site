c6c02cac7eb1d2367e6f84baaf69ec901477016d
zieee2025 <appy98834@gmail.com>
2025-12-14 19:46:05 +0800

    Update Wordle.tsx

--- src/components/wordle/Wordle.tsx
+++ src/components/wordle/Wordle.tsx
@@ -214,7 +214,7 @@ const ROWS = [TOP_ROW, MIDDLE_ROW, BOTTOM_ROW];
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const Wordle: React.FC<WordleProps> = () => {
-    const word = 'HENRY';
+    const word = 'PEARL';
     const [guesses, setGuesses] = useState<string[]>([]);
     const [gameOver, setGameOver] = useState(false);
     const [won, setWon] = useState(false);
