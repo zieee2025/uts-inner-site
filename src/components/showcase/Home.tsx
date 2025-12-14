5ac84df6a24182d36abc84aed92e3d32e9e78d40
zieee2025 <appy98834@gmail.com>
2025-12-14 19:05:46 +0800

    Update Home.tsx

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -16,8 +16,8 @@ const Home: React.FC<HomeProps> = (props) => {
     return (
         <div style={styles.page}>
             <div style={styles.header}>
-                <h1 style={styles.name}>Henry Heffernan</h1>
-                <h2>Software Engineer</h2>
+                <h1 style={styles.name}>Fritzie Jose</h1>
+                <h2>Civil Engineering Student</h2>
             </div>
             <div style={styles.buttons}>
                 <Link containerStyle={styles.link} to="about" text="ABOUT" />
