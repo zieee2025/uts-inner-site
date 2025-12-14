06c15855f4385abd24e24f1d2d68ec2edcb67b79
zieee2025 <appy98834@gmail.com>
2025-12-14 19:25:07 +0800

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -12,23 +12,21 @@ const About: React.FC<AboutProps> = (props) => {
         <div className="site-page-content">
             {/* <img src={me} style={styles.topImage} alt="" /> */}
             <h1 style={{ marginLeft: -16 }}>Welcome</h1>
-            <h3>I'm Henry Heffernan</h3>
+            <h3>I'm Fritzie Athea G. Jose</h3>
             <br />
             <div className="text-block">
                 <p>
-                    I'm a software engineer currently working at Vercel! In May
-                    of 2022 I graduated from Rensselaer Polytechnic Institute
-                    with my BS in Computer Science.
+                    Currently pursuing my degree in Civil Engineering at Isabela State University (ISU) - Echague.
                 </p>
                 <br />
                 <p>
-                    Thank you for taking the time to check out my portfolio. I
+                    Thank you for taking the time to check out my website. I
                     really hope you enjoy exploring it as much as I enjoyed
                     building it. If you have any questions or comments, feel
                     free to contact me using{' '}
                     <Link to="/contact">this form</Link> or shoot me an email at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:fritzieathea.g.jose@isu.edu.ph">
+                        fritzieathea.g.jose@isu.edu.ph"
                     </a>
                 </p>
             </div>
@@ -37,53 +35,24 @@ const About: React.FC<AboutProps> = (props) => {
                 <h3>About Me</h3>
                 <br />
                 <p>
-                    From a young age, I have had a curiosity about how things
-                    worked. This naturally led me to become absolutely obsessed
-                    with Lego and I fell in love with building things. In
-                    elementary school, I joined the Lego Robotics team at my
-                    local middle school, which was my first real exposure to
-                    programming. In 2008, my family and I moved across the
-                    country from California to New York, where I attended middle
-                    school, high school, and college.
+                    I tend to be the quiet observer in the room; I really need to take things in and think them over carefully before I jump in. It’s important to me that things are done well—that they feel truly correct—so I often find myself running things through my head a couple of extra times. It’s just how I ensure that everything is solid before I put it out there.
                 </p>
                 <br />
                 <div className="captioned-image">
                     <img src={me} style={styles.image} alt="" />
                     <p>
                         <sub>
-                            <b>Figure 1:</b> A real photo of me developing this
-                            website :)
+                            <b>Figure 1:</b> Kid :)
                         </sub>
                     </p>
                 </div>
 
                 <p>
-                    I started programming more seriously in high school,
-                    initially learning how to scrape and interact with websites.
-                    I went on to do a ton of passion projects, many of them with
-                    one of my closest friends,{' '}
-                    <a
-                        rel="noreferrer"
-                        target="_blank"
-                        href="https://www.linkedin.com/in/scott-bass-189a7919b/"
-                    >
-                        Scott Bass
-                    </a>
-                    . We worked on many projects together, including chat bots,
-                    multiple game projects, apps, and more. One of these
-                    projects is viewable on my{' '}
-                    <Link to="/projects/software">Software Projects</Link> page.
+                    WIP.
                 </p>
                 <br />
                 <p>
-                    In 2017, I got accepted into Rennselear Polytechnic
-                    Institute to study Computer Science. It was my first choice
-                    and I was absolutely ecstatic to be going to such a great
-                    university. At the end of my sophomore year, I got an
-                    internship working for the startup Hover, primarily focusing
-                    on frontend work. I continued to work at Hover on and off
-                    for about a year and a half, until the start of my senior
-                    year when I decided to focus on other opportunities.
+                    WIP
                 </p>
                 <br />
                 <br />
@@ -101,22 +70,12 @@ const About: React.FC<AboutProps> = (props) => {
                         <p>
                             Beyond software, I have a lot of hobbies that I
                             enjoy doing in my free time. The more tangible
-                            hobbies I have are{' '}
-                            <Link to="/projects/music">Music Production</Link>{' '}
-                            and creating{' '}
-                            <Link to="/projects/art">Digital Art</Link>. You can
-                            read more about each of these on their respective
-                            pages under my projects tab. Some other hobbies I
-                            enjoy are working out, cooking, and (unsurprisingly)
+                            hobbies I have are playing Guitar
+                            and creating Digital Art. Some other hobbies I
+                            enjoy are recycling, cooking, and (unsurprisingly)
                             playing video games.
                         </p>
                         <br />
-                        <p>
-                            In college, I was an active member in the fraternity
-                            Sigma Alpha Epsilon and held multiple positions in
-                            the chapter. I met a lot of amazing people through
-                            my fraternity and thoroughly enjoyed the community.
-                        </p>
                     </div>
                     <div style={styles.verticalImage}>
                         <img src={meNow} style={styles.image} alt="" />
@@ -131,15 +90,15 @@ const About: React.FC<AboutProps> = (props) => {
                 <br />
                 <p>
                     Thanks for reading about me! I hope that you enjoy exploring
-                    the rest of my portfolio website and everything it has to
+                    the rest of my website and everything it has to
                     offer. If you find the easter egg make sure to let me know
-                    on twitter{' '}
+                    on instagram{' '}
                     <a
                         rel="noreferrer"
                         target="_blank"
-                        href="https://twitter.com/henryheffernan"
+                        href="https://instagram.com/fritziej0113"
                     >
-                        @henryheffernan
+                        @fritziej0113
                     </a>{' '}
                     Good luck and have fun!
                 </p>
@@ -149,8 +108,8 @@ const About: React.FC<AboutProps> = (props) => {
                     them. You can reach me through the{' '}
                     <Link to="/contact">contact page</Link> or shoot me an email
                     at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:fritzieathea.g.jose@isu.edu.ph">
+                        fritzieathea.g.jose@isu.edu.ph
                     </a>
                 </p>
             </div>
