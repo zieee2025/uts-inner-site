687f34f4ac0769ce2261521f0056e19c54b522d5
zieee2025 <appy98834@gmail.com>
2025-12-14 22:05:03 +0800

    Update ResumeDownload.tsx

--- src/components/showcase/ResumeDownload.tsx
+++ src/components/showcase/ResumeDownload.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import printer from '../../assets/resume/printer.gif';
-import Resume from '../../assets/resume/Henry_Heffernan_S22_Resume.pdf';
+import Resume from '../../assets/resume/MAI_Fritzie_Jose.pdf';
 
 export interface ResumeDownloadProps {
     altText?: string;
